6dcfc9e1d8b41fc841149fdb50030af7235b903c
mark19992 <maturanmark1999@gmail.com>
2025-03-17 21:35:11 +0800

    ongoing update

---
+++ resources/views/components/basecomponent.blade.php
@@ -0,0 +1,52 @@
+<!--
+=========================================================
+* Material Kit 3 - v3.1.0
+=========================================================
+
+* Product Page:  https://www.creative-tim.com/product/material-kit 
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
+* Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/WebPage">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <title>
+    {{ config('app.name') }}
+  </title>
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
+  
+
+
+  <!-- CSS Files -->
+  <link id="pagestyle" href="{{ asset('thirdparty/material-kit') }}/css/material-kit.css?v=3.1.0" rel="stylesheet" />
+    <!--   Core JS Files   -->
+  <script src="{{ asset('thirdparty/material-kit') }}/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="{{ asset('thirdparty/material-kit') }}/js/core/bootstrap.min.js" type="text/javascript"></script>
+  <script src="{{ asset('thirdparty/material-kit') }}/js/plugins/perfect-scrollbar.min.js"></script>
+  <!--  Plugin for TypedJS, full documentation here: https://github.com/inorganik/CountUp.js -->
+  <script src="{{ asset('thirdparty/material-kit') }}/js/plugins/countup.min.js"></script>
+  <!-- Control Center for Material UI Kit: parallax effects, scripts for the example pages etc -->
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTTfWur0PDbZWPr7Pmq8K3jiDp0_xUziI"></script>
+  <script src="{{ asset('thirdparty/material-kit') }}/js/material-kit.min.js?v=3.1.0" type="text/javascript"></script>
+</head>
+
+<body class="bg-gray-700">
+  <nav class="navbar navbar-expand-lg bg-gray-400 text-dark rounded mx-5 mt-1">
+
+  
+  
+  </nav>
+
+</body>
+
+</html>
---
+++ resources/views/pages/landing.blade.php
@@ -0,0 +1,11 @@
+<x-basecomponent>
+
+
+
+
+
+
+
+    
+
+</x-basecomponent>
